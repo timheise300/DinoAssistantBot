b19a2821eace3e4d2f8689c65d98c4a74b86da1b
timheise300 <timheise300@gmail.com>
2021-07-27 13:30:04 -0400

    extracted out message handling for easier local testing

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #Python libraries that we need to import for our bot
 import random
 import os
+import responseutils
 from flask import Flask, request
 from pymessenger.bot import Bot
 
@@ -42,35 +43,9 @@ def verify_fb_token(token_sent):
 
 def get_message(message):
     if "!hatch" in message:
-        return hatch_dino(message)
+        return responseutils.hatch_dino(message)
     elif "!color" in message:
-        return randomize_color()
-
-def hatch_dino(message):
-        maxLevel = [int(i) for i in message.split() if i.isdigit()]
-        if len(maxLevel) > 0:
-            dinoLevel = random.randrange(5, maxLevel[0])
-            spawnLevel = dinoLevel / 1.5
-            dinoCount = get_dino_count()
-            response = "You had " + str(dinoCount) + " babies, level " + str(dinoLevel) + ", spawn level " + str(int(spawnLevel))
-            return response
-        else:
-            return "Unable to process your request, try again in format '!hatch <maxLevel>"
-
-def randomize_color():
-    regionRoll = random.randrange(0,5)
-    colorRoll = random.randrange(1,226)
-    response = "Region " + str(regionRoll) + ", color " + str(colorRoll)
-    return response
-
-def get_dino_count():
-    roll = random.randrange(1, 100)
-    if roll in range(1,3):
-        return 3
-    elif roll in range(4,13):
-        return 2
-    else:
-        return 1
+        return responseutils.randomize_color()
 
 #uses PyMessenger to send response to user
 def send_message(recipient_id, response):
