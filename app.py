6b295ac0a6ef98423df48c9702587719d2b1fb68
timheise300 <timheise300@gmail.com>
2021-07-27 09:09:45 -0400

    Added color randomizer option

--- app.py
+++ app.py
@@ -40,10 +40,13 @@ def verify_fb_token(token_sent):
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
-
-#chooses a random message to send to the user
 def get_message(message):
     if "!hatch" in message:
+        return hatch_dino(message)
+    elif "!color" in message:
+        return randomize_color()
+
+def hatch_dino(message):
         maxLevel = [int(i) for i in message.split() if i.isdigit()]
         if len(maxLevel) > 0:
             dinoLevel = random.randrange(5, maxLevel[0])
@@ -54,11 +57,17 @@ def get_message(message):
         else:
             return "Unable to process your request, try again in format '!hatch <maxLevel>"
 
+def randomize_color():
+    regionRoll = random.randrange(0,5)
+    colorRoll = random.randrange(1,226)
+    response = "Region " + str(regionRoll) + ", color " + str(colorRoll)
+    return response
+
 def get_dino_count():
     roll = random.randrange(1, 100)
-    if roll in range(1,2):
+    if roll in range(1,3):
         return 3
-    elif roll in range(3,12):
+    elif roll in range(4,13):
         return 2
     else:
         return 1
